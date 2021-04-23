c6d93c761bbb0e40d376d1c64a1b95c5cc198094
wibeunltd <wibe@hotmail.fr>
2021-04-23 23:13:45 +0200

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+// Déclaration et utilisation du module standard http pour créer le serveur HTTP
+const http = require('http');
+
+// Déclaration de la constante app, pour la création de l'instance de l'application
+const app = require('./app');
+
+// Configuration du port d'écoute
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// Lancement serveur
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+// Port d'écoute du serveur
+server.listen(port);
