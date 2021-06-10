e4bd1b889f88759792b2d85d65f297aebada99ab
wibeunltd <wibe@hotmail.fr>
2021-06-10 03:59:30 +0200

    création user, post et comment

---
+++ backend/migrations/20210610011311-create-user.js
@@ -0,0 +1,63 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(191)
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(191)
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(191)
+      },
+      lastLogin: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      biography: {
+        allowNull: true,
+        type: Sequelize.TEXT
+      },
+      isAdmin: {
+        allowNull: false,
+        default: false,
+        type: Sequelize.BOOLEAN
+      },
+      companyServices: {
+        allowNull: true,
+        type: Sequelize.STRING(191)
+      },
+      coverPicture: {
+        allowNull: true,
+        type: Sequelize.STRING(191)
+      },
+      profilePicture: {
+        allowNull: true,
+        type: Sequelize.STRING(191)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
