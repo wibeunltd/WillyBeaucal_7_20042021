0ba243497e8180b29918d87be27b3af10bf01a21
wibeunltd <wibe@hotmail.fr>
2021-06-11 01:04:37 +0200

    Désinstallation de la dépendance mask-data

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -1,5 +1,4 @@
 // Déclarations des modules requis
 const bcrypt    = require('bcrypt');
 const jwt       = require('jsonwebtoken');
-const MaskData  = require('maskdata');
 const User      = require('../models/user');
