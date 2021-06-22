26febcca03cd1c2786be27004e57b050e6e59051
wibeunltd <wibe@hotmail.fr>
2021-06-22 23:47:09 +0200

    Requête cors, restriction localhost:8080

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,23 @@
 // Déclarations des modules requis 
 const express = require('express');
-const helmet = require('helmet');
-const path = require('path');
+const helmet  = require('helmet');
+const path    = require('path');
 
 // Sécurisation des variables d'environnement par un stockage séparé
 require('dotenv').config();
 
+// Déclarations des routes
+const userRoutes  = require('./routes/userRoutes');
+
 // Création de l'instance de l'application
 const app = express();
-
-// Test routes
-app.use('/', (req, res, next) => {
-  console.log('ok');
-res.status(200).json({ message: 'Requête bien reçue !' })
-});
 
 // Sécurisation contre les attaques XSS
 app.use(helmet());
 
 // Permission des requêtes CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
@@ -29,5 +26,11 @@ app.use((req, res, next) => {
 // Extraction des données JSON du corps de requête
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Gestion du dossier de sauvegarde des images uploadées
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Routes utilisateurs
+app.use('/api/users/', userRoutes);
 
 module.exports = app;
