b71c5ab4e3cc856204f2f9af7a10fef610b5821e
wibeunltd <wibe@hotmail.fr>
2021-06-22 23:48:12 +0200

    Ajout de routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,17 @@ const router    = express.Router();
 // Déclaration et import du controller user
 const userCtrl = require('../controllers/userCtrl');
 
+
 // Gestion de l'entrée d'un mot de passe fort
-const pwd = require('../middleware/password-validator')
+
 
 // Gestion du nombre de requêtes utilisateurs
 const accountLimiter = require('../middleware/account-limit-requests');
 
 // Routes users
-router.post('/register', pwd.validation, accountLimiter)
-router.post('/login', accountLimiter)
+router.post('/register', userCtrl.register);
+router.post('/login', userCtrl.login);
+router.get('/profile', userCtrl.getUser);
+router.put('/profile', userCtrl.updatetUser);
+
+module.exports = router;
