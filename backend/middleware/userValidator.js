3a043d5bf171c49b50acaf0fa5e4fbd958b837ca
wibeunltd <wibe@hotmail.fr>
2021-08-22 00:22:47 +0200

    Update userValidator.js
    - Modification des textes de règles de validation pour l'enregistrement

--- backend/middleware/userValidator.js
+++ backend/middleware/userValidator.js
@@ -5,23 +5,23 @@ const { body, validationResult } = require('express-validator');
 exports.registerValidationRules = () => {
     return [
         body('firstname')
-            .notEmpty().withMessage('Le prénom est obligatoire, le champ ne peut pas être vide.')
-            .isLength({ min: 3, max: 40 }).withMessage('Le prénom doit contenir entre 3 et 40 caractères.')
-            .custom((val) => /^[A-Za-zÀ-ÖØ-öø-ÿ-\s]+$/i.test(val)).withMessage('Le prénom ne peut contenir que des lettres, des espaces, des traits d\'union.'),
+            .notEmpty().withMessage(`Le prénom est une propriété requise, il ne peut pas être vide.`)
+            .isLength({ min: 3, max: 40 }).withMessage(`Le prénom doit contenir entre 3 et 40 caractères.`)
+            .custom((val) => /^[A-Za-zÀ-ÖØ-öø-ÿ-\s]+$/i.test(val)).withMessage(`Le prénom ne peut contenir que des lettres, des espaces et des traits d'union.`),
         body('lastname')
-            .notEmpty().withMessage('Le nom est obligatoire, le champ ne peut pas être vide.')
-            .isLength({ min: 3, max: 40 }).withMessage('Le nom doit contenir 3 et 40 caractères.')
-            .custom((val) => /^[A-Za-zÀ-ÖØ-öø-ÿ-\s]+$/i.test(val)).withMessage('Le nom ne peut contenir que des lettres, des espaces, des traits d\'union.'),
+            .notEmpty().withMessage(`Le nom est une propriété requise, il ne peut pas être vide.`)
+            .isLength({ min: 3, max: 40 }).withMessage(`Le nom doit contenir entre 3 et 40 caractères.`)
+            .custom((val) => /^[A-Za-zÀ-ÖØ-öø-ÿ-\s]+$/i.test(val)).withMessage(`Le nom ne peut contenir que des lettres, des espaces et des traits d'union.`),
         body('email')
-            .notEmpty().withMessage('L\'email est obligatoire, le champ ne peut pas être vide.')
-            .isEmail().withMessage('Merci de saisir une adresse email valide'),
+            .notEmpty().withMessage(`L'email est une propriété requise, il ne peut pas être vide.`)
+            .isEmail().withMessage(`L'adresse email saisie, n'est pas une adresse valide. Merci de saisir une adresse mail valide.`),
         body('password')
-            .notEmpty().withMessage('Le mot de passe est obligatoire, le champ ne peut pas être vide.')
-            .isLength({ max: 32 }).withMessage('Le mot de passe ne peut pas excéder 32 caractères.')
-            .isStrongPassword().withMessage('Le mot de passe doit être fort : soit 8 caractères minimum, 1 majuscule minimum,  1 symbole minimum, 1 chiffre minimum'),
+            .notEmpty().withMessage(`Le mot de passe est une propriété requise, il ne peut pas être vide.`)
+            .isLength({ max: 32 }).withMessage(`Le mot de passe ne peut pas excéder 32 caractères.`)
+            .isStrongPassword().withMessage(`Votre mot de passe doit être fort. Il doit contenir au minimum 8 caractères dont 1 majuscule minimum, 1 symbole minimum et 1 chiffre minimum.`),
         body('pwdConfirm')
-            .notEmpty().withMessage('La confirmation du mot de passe est obligatoire, le champ ne peut pas être vide.')
-            .custom((value, { req }) => value === req.body.password).withMessage('Le mot de passe de confirmation doit être identique au mot de passe entré.'),
+            .notEmpty().withMessage(`La confirmation du mot de passe est une propriété requise, il ne peut pas être vide.`)
+            .custom((value, { req }) => value === req.body.password).withMessage(`Le mot de passe de confirmation ne correspond pas. Merci de saisir à l'identique votre mot de passe.`),
     ]
 };
 
