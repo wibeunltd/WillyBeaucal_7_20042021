2c94118288488850bbf9a825966e868f3d262500
wibeunltd <wibe@hotmail.fr>
2021-08-22 00:21:41 +0200

    Update date.js
    - Gestion date avec la dépendance dateFormat

--- backend/middleware/date.js
+++ backend/middleware/date.js
@@ -1,21 +1,5 @@
-// Gestion de la date
-const date1 = new Date();
+// Module requis
+const dateFormat = require('dateformat');
+const dateNow = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
 
-// Date format français
-const dateLocale = date1.toLocaleString('fr-FR',{
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-});
-
-// Heure format français
-const hourLocale = date1.toLocaleString('fr-FR', {
-    hour: 'numeric',
-    minute: 'numeric',
-});
-
-const localeDate = `le ${dateLocale} à ${hourLocale}.`;
-
-return localeDate;
-
+module.exports = dateNow
