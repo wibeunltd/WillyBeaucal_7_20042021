8a6485e3cda353d533efab934e4ebb8e317b84b5
wibeunltd <wibe@hotmail.fr>
2021-08-23 21:38:04 +0200

    Create userCtrl copy.js
    Copy de secours du controller user, en vue de modification

---
+++ backend/controllers/userCtrl copy.js
@@ -0,0 +1,181 @@
+// Modules requis
+const bcrypt        = require('bcrypt');
+const fs            = require('fs');
+const { User }      = require('../models');
+const dateFormat    = require('dateformat');
+
+// Variable d'environnement
+require('dotenv').config();
+
+/** Inscription d'un nouvel utilisateur
+ * @type {{firstname: string, lastname: string, email: string, password: string}}
+ * */ 
+exports.register = (req, res) => {
+    // Champs requête
+    const { firstname, lastname, email, password } = req.body;
+
+    // Checking
+    User.findOne({
+        attributes: ['email'],
+        where: { email: email }
+    })
+    .then(user => {
+        // Gestion administrateur 
+        const role = (email == 'hello@groupomania.fr') ? true : false;
+
+        if(!user) {
+            bcrypt.hash(password, 10)
+            .then(hash => {
+                User.create({
+                    firstname       : firstname,
+                    lastname        : lastname,
+                    email           : email,
+                    password        : hash,
+                    coverPicture    : `https://picsum.photos/1000/500`,
+                    profilePicture  : `https://eu.ui-avatars.com/api/?background=random&name=${firstname}+${lastname}`,
+                    isAdmin         : role,
+                    lastLogin       : dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
+                })
+                .then(newUser =>{
+                    fs.mkdir((`./public/users/${newUser.id}`), {recursive:true}, error =>{
+                        if(error) {
+                            return console.error(error);
+                        }
+                        const message = `L'inscription de l'utilisateur ${firstname} ${lastname} a aboutie avec succès.`
+                        return res.status(201).json({ message })
+                    })
+                })
+                .catch(error => {
+                    const message = `L'inscription n'a pas pu aboutir correctement. Merci de réessayer dans quelques instants.`
+                    return res.status(500).json({ message, data: error })
+                });
+            })
+            .catch(error => {
+                const message = `Un problème serveur, ne permet pas la finalisation de l'inscription. Merci de réessayer dans quelques instants.`
+                return res.status(500).json({ message, data: error })
+            });
+
+        } else {
+            const message = `L'adresse email saisie ne peut pas être utilisée. Merci d'en choisir une autre.`
+            return res.status(409).json({ message })
+        }
+    })
+    .catch(error => {
+        const message = `Un problème serveur, ne permet pas la verification du statut d'inscription. Merci de réessayer dans quelques instants.`
+        return res.status(500).json({ message, data: error })
+    });
+};
+
+
+/** Connexion d'un utilisateur existant
+ * @type {{email: string, password: string}} 
+*/
+exports.login = (req, res) => {
+
+    /** Champs requête */
+    const { email, password } = req.body;
+    
+    /** Checking */
+    User.findOne({
+        where: { email: email }
+    })
+    .then(user => {
+        if(user) {
+            bcrypt.compare(password, user.password)
+            .then(valid => {
+                if (valid) {
+                    return res.status(200).json({
+                        'ID'          : user.id,
+                        'Lastname'    : user.lastname,
+                        'Firstname'   : user.firstname,
+                        'Last Login'  : dateFormat(user.lastLogin, "dd-mm-yyyy HH:MM:ss"),
+                        'Created at'  : dateFormat(user.createdAt, "dd-mm-yyyy HH:MM:ss"),
+                        'Updated at'  : dateFormat(user.updatedAt, "dd-mm-yyyy HH:MM:ss"),
+                        //'Token'     : accesToken
+                    });
+                } else {
+                    const message = `Les informations d'identifications fournies sont invalides. Merci de vérifier vos saisies.`
+                    return res.status(401).json({ message })
+                }
+            })
+            .catch(error => {
+                const message = `Échec de la connexion, impossible d'accéder aux services en ligne. Merci de réessayer dans quelques instants.`
+                return res.status(500).json({ message, data: error })
+            })
+        } else {
+            const message = `Les informations d'identifications fournies sont invalides. Merci de vérifier vos saisies.`
+            return res.status(404).json({ message })
+        }
+    })
+    .catch(error => {
+        const message = `Échec de la connexion, impossible d'accéder aux services en ligne. Merci de réessayer dans quelques instants.`
+        return res.status(500).json({ message, data: error })
+    });
+};
+
+/** Récupération d'un profil utilisateur
+ * @param {{userId}}
+ * @param {{token}}
+*/
+exports.getUser = (req, res, next) => {
+    /** Récupération et vérification du token d'authentification */ 
+    const headerAuth  = req.headers['authorization'];
+    const userId      = userAuth.getUserId(headerAuth);
+
+    if (userId < 0)
+      return res.status(400).json({ 'error': 'Token non valide' });
+
+    User.findOne({
+      attributes: [ 'id', 'firstname', 'lastname', 'email', 'lastLogin', 'biography', 'isAdmin', 'companyServices', 'coverPicture', 'profilePicture', 'createdAt', 'updatedAt' ],
+      where: { id: userId }
+    }).then(function(user) {
+      if (user) {
+        res.status(201).json(user);
+      } else {
+        res.status(404).json({ 'error': 'Utilisateur non trouvé' });
+      }
+    }).catch(function(err) {
+      res.status(500).json({ 'error': 'Problème de connexion au serveur, impossible de récupérer les informations de l\'utilisateur' });
+    });
+};
+
+/** Mise à jour d'un profil utilisateur
+ @param {{userId}}
+ @param {{token}}
+*/
+exports.updateUser = (req, res, next) => {
+    /** Récupération et vérification du token d'authentification */
+    const headerAuth  = req.headers['authorization'];
+    const userId      = userAuth.getUserId(headerAuth);
+
+    /** Informations utilisateur */
+    const { firstname, lastname, email, password, biography, companyServices, coverPicture, profilePicture } = req.body;
+
+    User.findOne({
+        attributes: [ 'id', 'firstname', 'lastname', 'email', 'biography', 'companyServices', 'coverPicture', 'profilePicture' ],
+        where: { id: userId }
+    })
+    .then(userFound => {
+        if(userFound) {
+            userFound.update({
+                firstname       : (firstname ? firstname : userFound.firstname),
+                lastname        : (lastname ? lastname : userFound.lastname),
+                email           : (email ? email : userFound.email),
+                password        : (password ? password : userFound.password),
+                biography       : (biography ? biography : userFound.biography),
+                companyServices : (companyServices ? companyServices : userFound.companyServices),
+                coverPicture    : (coverPicture ? coverPicture : userFound.coverPicture),
+                profilePicture  : (profilePicture ? profilePicture : userFound.profilePicture)
+            })
+            .then(userFound => {
+                return res.status(201).json(userFound);
+            })
+            .catch(error => {
+                return res.status(500).json({ 'error': 'Impossible de mettre à jour l\'utilisateur.' })
+            })
+        } else {
+            res.status(404).json({ 'error': 'Utilisateur non trouvé' });
+        }
+    })
+    .catch(err => res.status(500).json({ 'error': 'Problème de connexion au serveur, impossible de mettre à jour les informations de l\'utilisateur' }));
+};
